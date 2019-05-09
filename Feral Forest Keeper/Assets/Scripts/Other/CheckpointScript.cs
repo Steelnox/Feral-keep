b7a033c2c2b4afb4faae8fbac5154e9b746e6a6e
Steelnox <eric14pastor@gmail.com>
2019-05-09 03:15:18 +0200

    Checkpoint done

---
+++ Feral Forest Keeper/Assets/Scripts/Other/CheckpointScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    bool checkpointActivated;
+    public GameObject checkpoint;
+    public PlayerController player;
+    void Start()
+    {
+        checkpointActivated = false;
+        player = PlayerController.instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!checkpointActivated && other.tag == "Player")
+        {
+            checkpoint.transform.position = player.transform.position;
+            checkpointActivated = true;
+        }
+    }
+}
