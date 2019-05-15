b677fd923df99c3d5513c2a32b02be0a0e4d1dc7
XaviCoch <xcoch@edu.tecnocampus.cat>
2019-05-15 15:48:29 +0200

    XAVI
    New Height Diffuse Shader Implemented.
    Shader Control Script Created to control height (relative to player)

---
+++ Feral Forest Keeper/Assets/Scripts/FXs/ShadersControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadersControl : MonoBehaviour
+{
+    private float heightMin = 0.0f;
+    private float heightMax = 5.10f;
+
+    public MeshRenderer heightDegradateLightingShader_Material;
+
+    private float worldHeightRelativeToPlayer;
+
+    void Start()
+    {
+        heightDegradateLightingShader_Material = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //heightDegradateLightingShader_Material = gameObject.GetComponent<MeshRenderer>();
+        worldHeightRelativeToPlayer = PlayerAnimationController.instance.transform.position.y;
+
+        heightDegradateLightingShader_Material.material.SetFloat("_HeightMin", worldHeightRelativeToPlayer + heightMin);
+        heightDegradateLightingShader_Material.material.SetFloat("_HeightMax", worldHeightRelativeToPlayer + heightMax);
+    }
+}
